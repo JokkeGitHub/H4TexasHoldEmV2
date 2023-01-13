dbaa060a83a38ea289259ffcc1c1b82f6eb1ee54
Joachim Hansen <skraldespand94@hotmail.com>
2023-01-13 13:44:23 +0100

    Added Mailsender WebApi, javascript screenshot script, and websocket tutorial in progress

---
+++ H4TexasHoldEmV2/wwwroot/Scripts/Screenshot.js
@@ -0,0 +1,32 @@
+﻿
+function Takeshot() {
+    let div = document.getElementById('app');
+
+    html2canvas(div).then(
+        function (canvas) {
+            document.getElementById('output').appendChild(canvas);
+        });
+} 
+
+
+/*
+function DownloadImg() {
+    html2canvas(document.querySelector("#output")).then(canvas => {
+        saveAs(canvas.ToDataUrl(), 'screenshot.png');
+    })
+}
+
+function saveAs(uri, filename) {
+    var link = document.createElement('a');
+    if (typeof link.download === 'string') {
+        link.href = uri;
+        link.downlaod = filename;
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    }
+    else {
+        window.open(uri);
+    }
+}
+*/
---
+++ WebApi/Controllers/EmailController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MailKit.Net.Smtp;
+using MimeKit;
+using MimeKit.Text;
+using MailKit.Security;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmailController : ControllerBase
+    {
+        [HttpPost]
+        [Route("sendemail")]
+        public IActionResult SendEmail(string body)
+        {
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse("jokkesdummymail@gmail.com"));
+            email.To.Add(MailboxAddress.Parse("joac3146@zbc.dk"));
+            email.Subject = "Test Email Subject";
+            email.Body = new TextPart(TextFormat.Html) { Text = body };
+
+            using var smtp = new SmtpClient();
+            smtp.Connect("smtp.gmail.com", 587, SecureSocketOptions.StartTls);
+            smtp.Authenticate("jokkesdummymail@gmail.com", "annkmqtqqxvalncn");
+            smtp.Send(email);
+            smtp.Disconnect(true);
+
+            return Ok();
+        }
+    }
+}
