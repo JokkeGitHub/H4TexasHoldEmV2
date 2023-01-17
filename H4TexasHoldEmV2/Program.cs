22c08eb6289a9f9551f2cadde6d4d9a72da1cad9
Joachim Hansen <skraldespand94@hotmail.com>
2023-01-17 20:44:57 +0100

    Fixed "ghost error" bug, by removing unused & unfinished code

--- H4TexasHoldEmV2/Program.cs
+++ H4TexasHoldEmV2/Program.cs
@@ -8,6 +8,4 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-builder.Services.AddWebSocketManager();
-
 await builder.Build().RunAsync();
